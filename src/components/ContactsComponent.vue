76947809dcf5b60aaa4610a23ad26237e9491dfe
nikitoda70 <nikitoda70@gmail.com>
2022-12-25 17:42:42 +0700

    feat: add contacts + adaptive to 992

--- src/components/ContactsComponent.vue
+++ src/components/ContactsComponent.vue
@@ -2,6 +2,28 @@
   <div class="contacts">
       <div class="container">
         <h2 class="block-title">{{ $t('titles.contacts') }}</h2>
+        <div class="contacts__wrapper">
+          <div class="contacts__img" v-scroll-animation>
+            <picture>
+              <source srcset="@/assets//img/contacts-mobile.png" media="(max-width: 768px)">
+              <img src="@/assets//img/contacts-desktop.png" alt="Me">
+            </picture>
+          </div>
+          <div class="contacts__content" v-scroll-animation>
+            <div class="contacts__item">
+              <span class="contacts__key">Github:&nbsp;</span>
+              <a href="https://github.com/nikitoda70" target="_blank" class="contacts__value">nikitoda70</a>
+            </div>
+            <div class="contacts__item">
+              <span class="contacts__key">Telegram:&nbsp;</span>
+              <a href="https://t.me/stfuchump" target="_blank" class="contacts__value">@stfuchump</a>
+            </div>
+            <div class="contacts__item">
+              <span class="contacts__key">{{ $t('contacts.mail') }}:&nbsp;</span>
+              <a href="mailto:nikitoda70@gmail.com" target="_blank" class="contacts__value">nikitoda70@gmail.com</a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,10 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    return { top: 0 }
+  },
 })
 
 export default router
